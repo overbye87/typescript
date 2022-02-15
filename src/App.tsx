ddd19a302414df16af70a4daf5428938974a5e7a
overbye <overbye87@gmail.com>
2022-02-15 11:45:34 +0300

    add fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import Card, {CardVariant} from "./components/Card";
 import UserList from "./components/UserList";
 import {IUser} from "./types/types";
+import axios from "axios";
 
 function App() {
-    const users: IUser[] = [
-        {
-            "id": 1, "name": "Leanne Graham", "email": "Sincere@april.biz",
-            "address": {
-                "street": "Kulas Light",
-                "city": "Gwenborough",
-                "zipcode": "92998-3874",
-            }
-        },
-        {
-            "id": 2, "name": "Antony Tompkins", "email": "asdfff@april.biz",
-            "address": {
-                "street": "Ferrr Grut",
-                "city": "Totenkolf",
-                "zipcode": "47282-7788",
-            }
-        },
-    ]
+    const [users, setUsers] = useState<IUser[]>([])
+
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+
+    async function fetchUsers() {
+        try {
+            const response = await axios.get<IUser[]>("https://jsonplaceholder.typicode.com/users")
+            setUsers(response.data)
+        } catch (e) {
+            alert(e)
+        }
+    }
 
     return (
         <div>
